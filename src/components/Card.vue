fe244c6b1ee98e392d8804d587661312c2ba8ea8
LucaVitale00 <luca16vitale2000@gmail.com>
2022-12-19 16:58:12 +0100

    i combine the card in a single file and fix a scroll bar

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,64 +1,74 @@
 <template>
-      <div class="card position-relative">
-          <img v-if="film.poster_path !== null " :src="'https://image.tmdb.org/t/p/' + 'w342' + film.poster_path" alt="">
-          <img v-else src="https://picsum.photos/194/291" alt="">
-          <div class="p-2 pos-absol">
-            <h4>{{film.title}}</h4>
-            <div class="mb-2 d-flex gap-2">language {{film.original_language}}<span :class="getFlag.icon"></span></div>
-            <div  class="mb-2">
-                <!-- metti fa-star se index é minore uguale del numero /2 (voto arrotondato)
+  <div class="card position-relative">
+    <img v-if="poster.poster_path" :src="'https://image.tmdb.org/t/p/' + 'w342' + poster.poster_path" alt="">
+    <img v-else src="https://picsum.photos/194/291" alt="">
+    <div class="p-2 pos-absol">
+      <h4>{{ poster.title ?? poster.name }}</h4>
+      <div class="mb-2 d-flex gap-2">language {{ poster.original_language }}<span :class="getFlag.icon"></span></div>
+      <div class="mb-2">
+        <!-- metti fa-star se index é minore uguale del numero /2 (voto arrotondato)
                  se minore O = metti fa (stella piena) SE maggiore metti fa-regular (stella vuota) -->
-                 <i class="text-warning fa-star" 
-                :class="num <= Math.round(film.vote_average / 2 )? 'fa':'fa-regular'
-                " v-for="num in 5"></i>
-            </div> 
-            <div class="fw-semibold mb-2"><small>{{film.overview}}</small></div>
-            <div class="fw-bold mb-2">original title: {{film.original_title}}</div>
-          </div>
+        <i class="text-warning fa-star" :class="num <= Math.round(poster.vote_average / 2) ? 'fa' : 'fa-regular'
+        " v-for="num in 5"></i>
       </div>
+      <div class="fw-semibold mb-2"><small>{{ poster.overview }}</small></div>
+      <div class="fw-bold mb-2">original title: {{ poster.original_title ?? poster.original_name }}</div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   props: {
-    film : {
+    poster: {
       type: Object,
     }
   },
 
   data() {
     return {
-      
+
     }
   },
-  computed : {
-    
-    getFlag(){
-           let toReturn = { icon :`fi fi-${this.film.original_language}`};
-           if (this.film.original_language == "en"){
-            toReturn = { icon: "fi fi-gb"}
-           }if (this.film.original_language == "ja"){
-            toReturn = { icon: "fi fi-jp"}
-           };
-           return  toReturn
-        },
+  computed: {
+
+    getFlag() {
+      let toReturn = { icon: `fi fi-${this.poster.original_language}` };
+      if (this.poster.original_language == "en") {
+        toReturn = { icon: "fi fi-gb" }
+      } if (this.poster.original_language == "ja") {
+        toReturn = { icon: "fi fi-jp" }
+      };
+      return toReturn
+    },
 
   }
 }
 </script>
 <style lang="scss">
-  .card{
+.card {
   overflow: hidden;
-  
-  img{
+
+  ::-webkit-scrollbar {
+    width: 7px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.804);
+    border-radius: 10px;
+
+  }
+
+  img {
     z-index: 2;
     display: block;
     max-width: 100%;
     height: 300px;
     object-fit: cover;
     object-position: center;
-    
-}
-.pos-absol{
+
+  }
+
+  .pos-absol {
     position: absolute;
     top: 0;
     right: 0;
@@ -67,22 +77,27 @@ export default {
     z-index: -1;
     overflow-y: hidden;
     color: white;
-    
+
   }
-&:hover{
-  overflow: auto;
-  img{
-    opacity: 0.6;
-    
-    
+
+  &:hover {
+    overflow: auto;
+
+    img {
+      opacity: 0.6;
+
+
+    }
+
+    .pos-absol {
+      z-index: 4;
+      overflow-y: auto;
+      background-color: rgba(0, 0, 0, 0.651)
+    }
+
   }
-  .pos-absol{
-    z-index: 4;
-    overflow-y: auto;
-    background-color: rgba(0, 0, 0, 0.651)
-  }
-  
+
 }
-  
-};
+
+;
 </style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,50 +1,51 @@
 <template>
   <section>
     <div class="container pb-5">
-      <h2 class=" py-4 text-white" v-if="store.show">films</h2>
+      <h2 class=" py-4 fs-1 text-white" v-if="store.show">Films</h2>
       <div class="row g-3 gy-5">
-        <div v-for="(film, index) in store.movies" :key="index" class="col-xl-2 col-lg-3 col-sm-4  d-flex flex-column">
-        <Card :film="film"></Card>
-         
+        <div v-for="(singlePoster, index) in store.movies" :key="index"
+          class="col-xl-2 col-lg-3 col-sm-4  d-flex flex-column">
+          <Card :poster="singlePoster"></Card>
+
         </div>
       </div>
 
-      <h2 class="py-4 text-white" v-if="store.show">Tv-Series</h2>
+      <h2 class="py-4 fs-1 text-white" v-if="store.show">Tv-Series</h2>
       <div class="row g-3 gy-5">
-       <div v-for="(series, index) in store.tvSeries" :key="index" class="col-xl-2 col-lg-3 col-sm-4 d-flex flex-column ">
-         <Card2 :series="series"></Card2>
-       </div>
+        <div v-for="(singlePoster, index) in store.tvSeries" :key="index"
+          class="col-xl-2 col-lg-3 col-sm-4 d-flex flex-column ">
+          <Card :poster="singlePoster"></Card>
+        </div>
       </div>
     </div>
   </section>
 </template>
 <script>
-import {store} from '../store'
+import { store } from '../store'
 import Card from '../components/Card.vue'
-import Card2 from '../components/Card2.vue'
 export default {
-  components : { Card, Card2,},
+  components: { Card, },
   data() {
     return {
       store,
-      
+
     }
   },
   methods: {
-    
+
   },
   mounted() {
-    
+
   },
 }
 </script>
 <style lang="scss">
+section {
+
+  width: 100%;
+  height: 100%;
 
 
-section{
-
-width: 100%;
-height: 100%;
 
 
 
